04290cb0318e2a8c5062e5fbfcc2e1c98e78d7a9
agusp19 <agusp.tkr3.04@gmail.com>
2022-09-16 09:37:28 +0700

    latihan Rea4a

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log ( " Halo , dunia " ) ;
+
+// kita harus kasih tau dulu ke git file apa yang ingin kita track perubahannya
+// git add <nama file>
+// git add .
+
+// kita ingin lihat a saja yang berubah
+// git status
+
+// git commit -m " ini adalah messagenya "
